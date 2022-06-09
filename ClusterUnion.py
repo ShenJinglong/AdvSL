2a4d317a0956f7db16302dee89c61e7d5ab49211
sjinglong <sjinglong31@163.com>
2022-06-09 11:15:35 +0800

    sync

--- ClusterUnion.py
+++ ClusterUnion.py
@@ -51,3 +51,16 @@ class ClusterUnion:
         anchor_loss.backward(retain_graph=True)
         [float_loss.backward(retain_graph=True) for float_loss in float_losses]
         self.__optim.step()
+
+    def update_gr_(self,
+        anchor_fm: torch.Tensor,
+        float_fms: List[torch.Tensor]
+    ) -> None:
+        self.__optim.zero_grad()
+        anchor_label = torch.ones((anchor_fm.size(0), 1), device=self.__device)
+        float_labels = [torch.zeros((float_fm.size(0), 1), device=self.__device) for float_fm in float_fms]
+        anchor_loss = self.__loss_fn(self.__discriminator(anchor_fm.detach()), anchor_label)
+        float_losses = [self.__loss_fn(self.__discriminator(float_fm), float_label) for float_fm, float_label in zip(float_fms, float_labels)]
+        total_loss = (anchor_loss + sum(float_losses)) / (1 + len(float_losses))
+        total_loss.backward(retain_graph=True)
+        self.__optim.step()
